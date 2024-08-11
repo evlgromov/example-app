1b7f74f3a8b48e8189b5620197110e7540c8d335
gromov <evgr89@yandex.ru>
2024-08-11 13:17:10 +0400

    СОздание таблицы проектов

---
+++ database/migrations/2024_08_11_091322_add_assignee_id_and_deadline_date_to_projects_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->integer('assignee_id')->nullable()->after('is_active');
+            $table->integer('deadline_date')->nullable()->after('assignee_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn(['assignee_id', 'deadline_date']);
+        });
+    }
+};
